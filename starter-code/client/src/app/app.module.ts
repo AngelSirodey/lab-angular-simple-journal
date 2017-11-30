f988b15e40db6a9c12625ab51b19b661a37b891d
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-11-30 16:15:29 +0100

    starting

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EntryLisComponent } from './entry-lis/entry-lis.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryLisComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
